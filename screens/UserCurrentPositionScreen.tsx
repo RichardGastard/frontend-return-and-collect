6263382da6fa74f714ddcd886e477b7ddaec005a
Mickaël Adiduyulmus <mickael.adiduyulmus@actelior.com>
2025-02-17 21:18:44 +0100

    user current position screen

--- screens/UserCurrentPositionScreen.tsx
+++ screens/UserCurrentPositionScreen.tsx
@@ -11,7 +11,11 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import Input from "@/components/Input";
 import CustomButton from "@/components/CustomButton";
 import Map from "@/components/Map";
-import DropdownMenu from "@/components/DropdownMenu";
+import ArrowBack from "@/components/ArrowBack";
+import "react-native-get-random-values";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+const GOOGLE_MAPS_APIKEY = "AIzaSyBAOlvP2wDhH_L_nljgCslBYcFptmRTtfM";
 
 export default function UserCurrentPositionScreen({ navigation }) {
   return (
@@ -21,18 +25,39 @@ export default function UserCurrentPositionScreen({ navigation }) {
           behavior={Platform.OS === "ios" ? "padding" : "height"}
           style={{ flex: 1 }}
         >
+          <View style={styles.back}>
+            <ArrowBack></ArrowBack>
+          </View>
           <Text style={styles.title}>Planifier votre délivraison</Text>
           <View style={styles.container}>
-            <Map
-              pickerPosition={{ latitude: 48.867, longitude: 2.313333 }}
-            ></Map>
-
-            <Input label="Numéro, rue" />
-            <Input label="Ville" />
-            <Input label="Code postal" />
-            <Input label="Etage" />
+            <Map></Map>
 
             <View style={styles.form}>
+              <GooglePlacesAutocomplete
+                placeholder="Addresse de délivraison"
+                onPress={(data, details = null) => {
+                  console.log(data, details);
+                }}
+                query={{
+                  key: GOOGLE_MAPS_APIKEY,
+                  language: "en",
+                }}
+                debounce={500}
+                styles={{
+                  textInputContainer: {
+                    backgroundColor: "#fffbf0",
+                    borderWidth: 1,
+                    borderRadius: 3,
+                    borderColor: "#525252",
+                  },
+                  textInput: {
+                    backgroundColor: "#fffbf0",
+                    borderRadius: 3,
+                  },
+                }}
+              />
+              <Input label="Etage" />
+
               <CustomButton
                 onPressFunction={() => {
                   navigation.navigate("TabNavigator");
@@ -73,13 +98,21 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   form: {
+    paddingBlock: 20,
     width: "100%",
     marginRight: "auto",
     marginLeft: "auto",
     marginTop: 30,
+    height: "40%",
   },
   footer: {
     alignItems: "center",
     marginTop: 20,
+  },
+  back: {
+    position: "absolute",
+    top: -5,
+    left: 15,
+    zIndex: 1000,
   },
 });
