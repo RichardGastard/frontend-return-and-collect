3c7376d57b7fe3431bdb9402ffe52c3a252cf82a
Marc Hourselle <hourselle.marc@gmail.com>
2025-02-26 21:33:56 +0100

    rajout de fetch dans la page pyckerpaymentmethod

--- screens/PickerPaymentMethodsScreen.tsx
+++ screens/PickerPaymentMethodsScreen.tsx
@@ -24,28 +24,49 @@ function PickerPayement({ navigation }) {
   const [iban, setIban] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [bic, setBic] = useState<string>("");
+  const [bankName, setbankName] = useState<string>("");
 
-  const [ibanNumber, setIbanNumber] = useState<boolean>(true);
-  const [bicNumber, setBicNumber] = useState<boolean>(true);
+  // const [ibanNumber, setIbanNumber] = useState<boolean>(true);
+  // const [bicNumber, setBicNumber] = useState<boolean>(true);
 
-  const handleSubmit = () => {
-    const ibanRegex =
-      /b[A-Z]{2}[0-9]{2}(?:[ ]?[0-9]{4}){4}(?!(?:[ ]?[0-9]){3})(?:[ ]?[0-9]{1,2})?b/;
-    const bicRegex = /^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?/;
+  // const handleSubmit = () => {
+  //   const ibanRegex =
+  //     /b[A-Z]{2}[0-9]{2}(?:[ ]?[0-9]{4}){4}(?!(?:[ ]?[0-9]){3})(?:[ ]?[0-9]{1,2})?b/;
+  //   const bicRegex = /^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?/;
 
-    if (!iban.match(ibanRegex) || iban.length < 2) {
-      setIbanNumber(false);
-      console.log("Iban incorrect");
-      return;
-    }
-    if (!bic.match(bicRegex) || bic.length < 2) {
-      setBicNumber(false);
-      console.log("Bic invalide");
-      return;
-    } else {
-      navigation.navigate("Validation"); // Penser à Changer la route ainsi que sur le bouton : "Passez cette étape" !!!
-    }
-  };
+  //   if (!iban.match(ibanRegex) || iban.length < 2) {
+  //     setIbanNumber(false);
+  //     console.log("Iban incorrect");
+  //     return;
+  //   }
+  //   if (!bic.match(bicRegex) || bic.length < 2) {
+  //     setBicNumber(false);
+  //     console.log("Bic invalide");
+  //     return;
+  //   } else {
+  //     navigation.navigate("Validation"); // Penser à Changer la route ainsi que sur le bouton : "Passez cette étape" !!!
+  //   }
+  // };
+
+  function handleRegistercreditMethod() {
+    fetch(process.env.EXPO_PUBLIC_BACKEND_URL + "/payments/ibanbic", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        bankName: bankName,
+        iban: iban,
+        bic: bic,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        // Envoie vers la page Account pour l'utilisateur puisse commpléter son profil
+        if (data.result) {
+          navigation.navigate("Validation");
+        }
+      });
+  }
 
   // !!!!! CE CODE NE PERMET PAS D'AFFICHER PLUSIEURS ERREURS SIMULTANEMENT !!!!!!!
 
@@ -80,6 +101,12 @@ function PickerPayement({ navigation }) {
               value={name}
             />
             <Input
+              label="Nom de la banque"
+              keyboardType="none"
+              onChangeText={(value) => setbankName(value)}
+              value={bankName}
+            />
+            <Input
               label="Iban"
               keyboardType="none"
               onChangeText={(value) => setIban(value)}
@@ -93,7 +120,7 @@ function PickerPayement({ navigation }) {
             />
             <CustomButton
               onPressFunction={() => {
-                handleSubmit();
+                handleRegistercreditMethod();
               }}
             >
               Validez les informations
