56fb9b1f8b0e6120d5bb7d3f9489162c077c0c53
Mickaël Adiduyulmus <mickael.adiduyulmus@actelior.com>
2025-03-06 20:59:23 +0100

    add sorting to activities and picker activity

--- screens/PickerActivityScreen.tsx
+++ screens/PickerActivityScreen.tsx
@@ -1,19 +1,69 @@
 // COMPONENTS
 import Layout from "@/components/Layout";
 import OrderCard from "@/components/OrderCard";
+import { useAppSelector } from "@/store/hooks";
 
 import { useSwipe } from "hook/useSwipe";
+import { useCallback, useEffect, useState } from "react";
 
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, RefreshControl } from "react-native";
 
 function PickerActivityScreen({ navigation }) {
+  const [deliveriesData, setDeliveriesData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const userData = useAppSelector((state) => state.users.value);
+
   const { onTouchStart, onTouchEnd } = useSwipe(onSwipeLeft, onSwipeRight, 3);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
 
   function onSwipeLeft() {
     navigation.navigate("Collect");
   }
 
   function onSwipeRight() {}
+
+  useEffect(() => {
+    fetch(
+      process.env.EXPO_PUBLIC_BACKEND_URL +
+        "/deliveries/pickerActivity/" +
+        userData.token
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setDeliveriesData(data.deliveries);
+      });
+  }, [refreshing]);
+
+  const deliveryCards = deliveriesData
+    .sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    })
+    .map((delivery, i) => {
+      const collector = delivery.senderId
+        ? delivery.senderId.firstName +
+          " " +
+          delivery.senderId.lastName.charAt(0).toUpperCase() +
+          "."
+        : "Collecteur non trouvé";
+      return (
+        <OrderCard
+          key={i}
+          orderNumber={delivery._id.substring(0, 5)}
+          location={delivery.pickupAddress}
+          collector={collector}
+          status={delivery === "DELIVERED"}
+          price={delivery.price}
+          date={delivery.createdAt.split("T")[0]}
+        ></OrderCard>
+      );
+    });
+
   return (
     <Layout
       title="Historique"
@@ -25,26 +75,16 @@ function PickerActivityScreen({ navigation }) {
         style={{ flex: 1 }}
       >
         <ScrollView
-          contentContainerStyle={{ flexGrow: 1 }}
+          contentContainerStyle={{ flexGrow: 1, paddingBottom: 10 }}
           keyboardShouldPersistTaps="handled"
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            ></RefreshControl>
+          }
         >
-          <View style={{ flex: 1, gap: 15 }}>
-            <OrderCard
-              orderNumber={395885}
-              location="3 rue des baies, 34555 CEDEX"
-              collector="Bob"
-              status
-              price={3.4}
-              date="05/09/2024"
-            ></OrderCard>
-            <OrderCard
-              orderNumber={23525}
-              location="23 avenue du général leclerc, 750001 Paris"
-              collector="Bob"
-              price={1.99}
-              date="21/02/2025"
-            ></OrderCard>
-          </View>
+          {deliveryCards}
         </ScrollView>
       </View>
     </Layout>
